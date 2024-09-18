2d674b27c5096b567b34368db85307ee51ebfbc3
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-09-18 09:15:43 -0300

    adiciona rota /sobre

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,12 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
 const routes = Router();
+
+routes.use('/sobre', (_, res) => {
+  res.status(200).json({
+    estudante: 'Augusto Preis Tomasi',
+    projeto: 'Encurtador de Links',
+  });
+});
 
 module.exports = { routes };
