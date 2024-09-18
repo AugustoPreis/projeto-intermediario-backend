1fe973b7f87c5d5a597c82725f183bc3a7d5c8ec
Augusto Tomasi <augustopreisthomasi@gmail.com>
2024-09-18 09:13:38 -0300

    adiciona parser de url e json do express

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ require('dotenv/config');
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use(express.json({ limit: '50mb' }));
+
 app.use(routes);
 
 app.use(errorHandler);
